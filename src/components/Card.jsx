a8104cf7289e4305c19169daf1cb78da870281d1
AngieGichane <angiegichane@gmail.com>
2025-07-03 17:34:19 +0300

    Rectified Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 function Card({ title, children }) {
   return (
-    <div className="border rounded-lg shadow p-4 bg-white">
-      <h2 className="text-xl font-bold mb-2">{title}</h2>
+    <div className="bg-white shadow-md rounded-lg p-4 dark:bg-gray-800 dark:text-white">
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
       <div>{children}</div>
     </div>
   );
