28a455b6bbe6f9bbff2678c653c41d08bd55cbcd
AngieGichane <angiegichane@gmail.com>
2025-07-03 17:42:40 +0300

    Fixed Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-function Layout({ children }) {
+function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow p-4">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
